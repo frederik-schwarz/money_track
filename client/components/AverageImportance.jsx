f6e836a849e32b863f12164f698703ff430ec168
Frederik Schwarz <frederik@jens-schwarz.com>
2021-08-10 12:02:45 +1200

    final colors added

--- client/components/AverageImportance.jsx
+++ client/components/AverageImportance.jsx
@@ -8,15 +8,19 @@ function TotalSpentDay (props) {
     let average = 0
     let length = props.purchase.length
     let decimalLimit = 0
+    let imp = 0
     useEffect(() => {
         props.dispatch(setAllPurchases()) 
     }, [])
+
+    props.purchase.map(purchase => {
+        imp = imp + purchase.importance
+       })
+    average = imp / length
+    decimalLimit = parseFloat(average).toFixed(1)
+
     return (
         <div className="averageContainer">
-             {props.purchase.map(purchase => {
-                 average = (average + purchase.importance) / length
-                 decimalLimit = parseFloat(average).toFixed(1)
-                })}
             <p className="average">Average importance: {decimalLimit}/10</p>
             
         </div>
